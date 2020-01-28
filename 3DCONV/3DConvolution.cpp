a9ff425938fcbfdc3b216a6795f19d8eede9a870
ncapodieci <nicola.capodieci@unimore.it>
2020-01-28 15:15:12 +0100

    [3DCONV] Added first draft

---
+++ 3DCONV/3DConvolution.cpp
@@ -0,0 +1,212 @@
+/**
+ * 2DConvolution.cpp: This file is part of the PolyBench/GPU 1.0 test suite,
+ * Vulkan version
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+
+#include "../vkcomp/VulkanCompute.h"
+#include "../vkcomp/stdafx.h"
+#include "../polybenchUtilFuncts.h"
+
+//define the error threshold for the results "not matching"
+#define PERCENT_DIFF_ERROR_THRESHOLD 0.5
+
+/* Problem size */
+#define NI 256
+#define NJ 256
+#define NK 256
+
+/* Thread block dimensions */
+#define DIM_THREAD_BLOCK_X 32
+#define DIM_THREAD_BLOCK_Y 8
+
+/* Can switch DATA_TYPE between float and double */
+typedef float DATA_TYPE;
+
+void conv3D(DATA_TYPE* A, DATA_TYPE* B)
+{
+	int i, j, k;
+	DATA_TYPE c11, c12, c13, c21, c22, c23, c31, c32, c33;
+
+	c11 = +2;  c21 = +5;  c31 = -8;
+	c12 = -3;  c22 = +6;  c32 = -9;
+	c13 = +4;  c23 = +7;  c33 = +10;
+
+	for (i = 1; i < NI - 1; ++i) // 0
+	{
+		for (j = 1; j < NJ - 1; ++j) // 1
+		{
+			for (k = 1; k < NK -1; ++k) // 2
+			{
+				//printf("i:%d\nj:%d\nk:%d\n", i, j, k);
+				B[i*(NK * NJ) + j*NK + k] = c11 * A[(i - 1)*(NK * NJ) + (j - 1)*NK + (k - 1)]  +  c13 * A[(i + 1)*(NK * NJ) + (j - 1)*NK + (k - 1)]
+					     +   c21 * A[(i - 1)*(NK * NJ) + (j - 1)*NK + (k - 1)]  +  c23 * A[(i + 1)*(NK * NJ) + (j - 1)*NK + (k - 1)]
+					     +   c31 * A[(i - 1)*(NK * NJ) + (j - 1)*NK + (k - 1)]  +  c33 * A[(i + 1)*(NK * NJ) + (j - 1)*NK + (k - 1)]
+					     +   c12 * A[(i + 0)*(NK * NJ) + (j - 1)*NK + (k + 0)]  +  c22 * A[(i + 0)*(NK * NJ) + (j + 0)*NK + (k + 0)]   
+					     +   c32 * A[(i + 0)*(NK * NJ) + (j + 1)*NK + (k + 0)]  +  c11 * A[(i - 1)*(NK * NJ) + (j - 1)*NK + (k + 1)]  
+					     +   c13 * A[(i + 1)*(NK * NJ) + (j - 1)*NK + (k + 1)]  +  c21 * A[(i - 1)*(NK * NJ) + (j + 0)*NK + (k + 1)]  
+					     +   c23 * A[(i + 1)*(NK * NJ) + (j + 0)*NK + (k + 1)]  +  c31 * A[(i - 1)*(NK * NJ) + (j + 1)*NK + (k + 1)]  
+					     +   c33 * A[(i + 1)*(NK * NJ) + (j + 1)*NK + (k + 1)];
+			}
+		}
+	}
+}
+
+
+void init(DATA_TYPE* A)
+{
+	int i, j, k;
+
+	for (i = 0; i < NI; ++i)
+    	{
+		for (j = 0; j < NJ; ++j)
+		{
+			for (k = 0; k < NK; ++k)
+			{
+				A[i*(NK * NJ) + j*NK + k] = i % 12 + 2 * (j % 7) + 3 * (k % 13);
+			}
+		}
+	}
+}
+
+
+void compareResults(DATA_TYPE* B, DATA_TYPE* B_outputFromGpu)
+{
+	int i, j, k, fail;
+	fail = 0;
+    int count = 0;
+	
+	// Compare result from cpu and gpu...
+	for (i = 1; i < NI - 1; ++i) // 0
+	{
+		for (j = 1; j < NJ - 1; ++j) // 1
+		{
+			for (k = 1; k < NK - 1; ++k) // 2
+			{
+                if(count%100==0) std::cout << "CCHECK [" << count << "] " << B[i*(NK * NJ) + j*NK + k] << "/" << B_outputFromGpu[i*(NK * NJ) + j*NK + k] << std::endl;
+                count++;
+				if (percentDiff(B[i*(NK * NJ) + j*NK + k], B_outputFromGpu[i*(NK * NJ) + j*NK + k]) > PERCENT_DIFF_ERROR_THRESHOLD)
+				{
+					fail++;
+				}
+			}	
+		}
+	}
+	
+	// Print results
+	printf("Non-Matching CPU-GPU Outputs Beyond Error Threshold of %4.2f Percent: %d\n", PERCENT_DIFF_ERROR_THRESHOLD, fail);
+}
+
+
+void GPU_argv_init(VulkanCompute *vk)
+{
+	vk->createContext();
+	vk->printContextInformation();
+}
+
+void convolution3DVulkan(DATA_TYPE* A, DATA_TYPE* B, DATA_TYPE* B_outputFromGpu)
+{
+	double t_start, t_end;
+
+    char buff[FILENAME_MAX];
+	GetCurrentDir( buff, FILENAME_MAX );
+	CrossFileAdapter crossFileAdapter;
+	std::string cwd(buff);
+
+	crossFileAdapter.setAbsolutePath(cwd+FILE_SEPARATOR+"conv3DKernel.comp");
+
+	if (vk->loadAndCompileShader(crossFileAdapter, "conv3DKernel") == -1)
+	{
+		std::cout << "Error in compiling shader conv3DKernel.comp" << std::endl;
+		exit(-1);
+	}
+
+	DATA_TYPE *A_gpu = (DATA_TYPE*) vk->deviceSideAllocation(sizeof(DATA_TYPE)*NI*NJ*NK, BufferUsage::BUF_INOUT);
+	DATA_TYPE *B_gpu = (DATA_TYPE*) vk->deviceSideAllocation(sizeof(DATA_TYPE)*NI*NJ*NK, BufferUsage::BUF_INOUT);
+
+    memcpy(A_gpu,A,sizeof(DATA_TYPE)*NI*NJ*NK);
+
+    vk->startCreateCommandList();
+		vk->synchBuffer(PPTR(A_gpu),HOST_TO_DEVICE);
+	vk->finalizeCommandList();
+	vk->submitWork();
+	vk->deviceSynch();
+
+	/*dim3 block(DIM_THREAD_BLOCK_X, DIM_THREAD_BLOCK_Y);
+	dim3 grid((size_t)(ceil( ((float)NK) / ((float)block.x) )), (size_t)(ceil( ((float)NJ) / ((float)block.y) )));*/
+    ComputeWorkDistribution_t block(DIM_THREAD_BLOCK_X, DIM_THREAD_BLOCK_Y);
+	ComputeWorkDistribution_t grid((size_t)(ceil( ((float)NK) / ((float)block.x) )), (size_t)(ceil( ((float)NJ) / ((float)block.y) )));
+
+    vk->startCreatePipeline("conv3DKernel");
+		vk->setArg(PPTR(A_gpu),"conv3DKernel",4);
+		vk->setArg(PPTR(B_gpu),"conv3DKernel",5);
+        vk.setSymbol(0, sizeof(int));
+		vk->setLaunchConfiguration(grid,block);
+	PIPELINE_HANDLE hPipeline = vk->finalizePipeline();
+
+    vk->startCreateCommandList();
+		vk->selectPipeline(hPipeline);
+        for(int i=0; i< NI-1; ++i){
+             vk->copySymbolInt(i,"conv3DKernel",0);
+             vk->launchComputation("conv3DKernel");
+        }
+    vk->finalizeCommandList();
+	vk->deviceSynch();
+
+	t_start = rtclock();
+    vk->submitWork();
+	vk->deviceSynch();
+	t_end = rtclock();
+	fprintf(stdout, "GPU Runtime: %0.6lfs\n", t_end - t_start);
+	
+    vk->startCreateCommandList();
+		vk->synchBuffer(PPTR(B_gpu),HOST_TO_DEVICE);
+	vk->finalizeCommandList();
+	vk->submitWork();
+	vk->deviceSynch();
+
+    memcpy(B_outputFromGpu,B_gpu,sizeof(DATA_TYPE) * NI * NJ * NK);
+}
+
+
+int main(int argc, char *argv[])
+{
+	double t_start, t_end;
+
+	DATA_TYPE* A;
+	DATA_TYPE* B;
+	DATA_TYPE* B_outputFromGpu;
+
+	A = (DATA_TYPE*)malloc(NI*NJ*NK*sizeof(DATA_TYPE));
+	B = (DATA_TYPE*)malloc(NI*NJ*NK*sizeof(DATA_TYPE));
+	B_outputFromGpu = (DATA_TYPE*)malloc(NI*NJ*NK*sizeof(DATA_TYPE));
+	
+	init(A);
+	
+    VulkanCompute vk;
+	GPU_argv_init(&vk);
+
+	convolution3DVulkan(A, B, B_outputFromGpu);
+
+	t_start = rtclock();
+	conv3D(A, B);
+	t_end = rtclock();
+	fprintf(stdout, "CPU Runtime: %0.6lfs\n", t_end - t_start);
+	
+	compareResults(B, B_outputFromGpu);
+
+	free(A);
+	free(B);
+	free(B_outputFromGpu);
+    vk.freeResources();
+
+    return EXIT_SUCCESS;
+}
